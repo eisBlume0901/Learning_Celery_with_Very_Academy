7775235e82e43600f243dc0614617378bbed42ee
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-11-14 20:12:08 +0800

    Added .env file, added redis and celery containers to docker-compose.yml. Added CELERY_BROKER_URL and CELERY_RESULT_BACKEND at settings.py

--- Learning_Celery_with_Very_Academy/settings.py
+++ Learning_Celery_with_Very_Academy/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND")
